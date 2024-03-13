7ea1320a45ff0994e341ba1c398b27aa5dcad183
AdjBecerra <romeroadj0507@gmail.com>
2024-03-13 11:16:39 -0300

    primer

---
+++ demo.js
@@ -0,0 +1,12 @@
+var nombre = 'Turi';
+
+function manejar(edad){
+    if (edad >= 18){
+        return nombre +  ':  puede y esta permitido manejar' + edad + ' años'
+
+    }else{
+        return nombre + 'Turi es menor de edad ';
+    }
+}
+
+manejar();
